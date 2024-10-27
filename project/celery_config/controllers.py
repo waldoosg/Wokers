029f4725b4b461d14ae36537858c38bfb47622b0
waldoosg <waldo.seguel@uc.cl>
2024-10-27 01:58:40 -0300

    probando ponderador_por_fixtures

--- project/celery_config/controllers.py
+++ project/celery_config/controllers.py
@@ -103,14 +103,15 @@ def ponderador_por_fixtures(id_usuario):
     for fixture in fixtures:
         round = int(fixture['league_round'].split(" ")[-1])
         sum_odds = 0
-        print("Fixture:", fixture)
         if fixture['home_team_id'] in aciertos:
+            print("entré 1")
             for odd in fixture['odds']:
                 if odd['value'] == 'Home':
                     sum_odds += float(odd['odd'])
             ponderador[fixture['home_team_id']] = aciertos[fixture['home_team_id']] * round / sum_odds
 
         if fixture['away_team_id'] in aciertos:
+            print("entré 2")
             for odd in fixture['odds']:
                 if odd['value'] == 'Away':
                     sum_odds += float(odd['odd'])
